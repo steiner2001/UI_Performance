5d30566671e45b6f06465bebd8432f5fbbeeed97
steiner2001 <steiner.akos.2001@gmail.com>
2021-08-02 11:18:28 +0200

    ember version update

---
+++ implementations/angular-11.0.5/src/app/store/index.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { cold } from 'jasmine-marbles';
+import { createStore } from './index';
+import { TodoStateInterface } from './todo-state.interface';
+import { onCreate } from './actions/todo.action';
+
+describe('createStore', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot(createStore())]
+    });
+  });
+
+  it('should create a new instance of store', () => {
+    const store: Store<TodoStateInterface> = TestBed.inject(Store);
+
+    store.dispatch(onCreate('Demo'));
+
+    const expectedTodos = cold('a', { a: [{ id: jasmine.any(String), name: 'Demo', completed: false }] });
+    expect(store.select('todos')).toBeObservable(expectedTodos);
+  });
+});
